f5d828ff2ae5d7701395d5f8e99b8de7a387807a
ZacharyLarouche <38225748+ZacharyLarouche@users.noreply.github.com>
2018-04-30 14:15:43 -0400

    Update cartrader.sql

--- cartrader.sql
+++ cartrader.sql
@@ -1,22 +1,21 @@
 -- BusinessProfile Table
 
 CREATE TABLE IF NOT EXISTS `BusinessProfile` (
-    `ID`     INT          NOT NULL,
+    `ID`                    INT          NOT NULL,
     `LoginId`               INT          NOT NULL,
     `BusinessName`          VARCHAR (50) NOT NULL,
+    `LocationId`            INT          NOT NULL,
     `Rating`                INT              NULL,
     `RatingCounter`         INT          NOT NULL,
     `Phone`                 VARCHAR (15) NOT NULL,
     `Email`                 VARCHAR (50) NOT NULL,
-    `Address`               VARCHAR (50) NOT NULL,
-    `Postal`                VARCHAR (7)  NOT NULL,
     PRIMARY KEY CLUSTERED (`ID` ASC)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 -- CarListing Table
 
 CREATE TABLE IF NOT EXISTS `CarListing` (
-    `ID` INT              NOT NULL,
+    `ID`           INT              NOT NULL,
     `CarMake`      VARCHAR (50)     NOT NULL,
     `CarModel`     VARCHAR (50)     NOT NULL,
     `CarTrim`      VARCHAR (50)     NOT NULL,
@@ -35,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `CarListing` (
 -- CarPartListing Table
 
 CREATE TABLE IF NOT EXISTS `CarPartListing` (
-    `ID` INT             NOT NULL,
+    `ID`               INT             NOT NULL,
     `PostDate`         DATE            NOT NULL,
     `Description`      VARCHAR (65535) NOT NULL,
     `LoginId`          INT             NOT NULL,
@@ -77,6 +76,15 @@ CREATE TABLE IF NOT EXISTS `Inbox` (
     PRIMARY KEY CLUSTERED (`ID` ASC)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
+-- Location Table
+
+CREATE TABLE IF NOT EXISTS `Location` (
+    `ID` INT             NOT NULL,
+    `Postal`     VARCHAR (7)     NOT NULL,
+    `Address`    VARCHAR (65535) NOT NULL,
+    PRIMARY KEY CLUSTERED (`ID` ASC)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
 -- Login Table
 
 CREATE TABLE IF NOT EXISTS `Login` (
@@ -102,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `Profile` (
 -- Region Table
 
 CREATE TABLE IF NOT EXISTS `Region` (
-    `RegionId`   INT          NOT NULL,
+    `ID`   INT          NOT NULL,
     `RegionName` VARCHAR (10)     NULL,
     PRIMARY KEY CLUSTERED (`ID` ASC)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
@@ -116,75 +124,128 @@ CREATE TABLE IF NOT EXISTS `Wishlist` (
     PRIMARY KEY CLUSTERED (`ID` ASC)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
+-- Add Auto-Incrementing for tables
+ALTER TABLE `businessprofile`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `carlisting`
+--
+ALTER TABLE `carlisting`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `carpartlisting`
+--
+ALTER TABLE `carpartlisting`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `inbox`
+--
+ALTER TABLE `inbox`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `region`
+--
+ALTER TABLE `region`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
 -- Add Unique to Login
 ALTER TABLE `Login`
-    ADD UNIQUE(`Username`); 
+    ADD UNIQUE(`Username`);
 
 -- Add Constraints to Tables
 
 -- BusinessProfile
 ALTER TABLE `BusinessProfile`
-    ADD CONSTRAINT `FK_BusinessProfileId_ToLogin` FOREIGN KEY (`LoginId`)
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_BusinessProfileId_ToLocation` FOREIGN KEY (`ID`)
+    REFERENCES `Location` (`ID`) ON DELETE CASCADE;
+
+ALTER TABLE `BusinessProfile`
+    ADD CONSTRAINT `FK_BusinessProfileId_ToLogin` FOREIGN KEY (`ID`)
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 -- CarListing
 ALTER TABLE `CarListing`
-    ADD CONSTRAINT `FK_CarListings_ToLogin` FOREIGN KEY (`LoginId`)
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_CarListings_ToLogin` FOREIGN KEY (`ID`)
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `CarListing`
-    ADD CONSTRAINT `FK_CarListings_ToRegion` FOREIGN KEY (`RegionId`)
-    REFERENCES `Region` (`RegionId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_CarListings_ToRegion` FOREIGN KEY (`ID`)
+    REFERENCES `Region` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `CarListing`
-    ADD CONSTRAINT `FK_CarListings_ToImage` FOREIGN KEY (`ImageId`) 
-    REFERENCES `Image` (`ImageId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_CarListings_ToImage` FOREIGN KEY (`ID`) 
+    REFERENCES `Image` (`ID`) ON DELETE CASCADE;
 
 -- CarPartListing
 ALTER TABLE `CarPartListing`
-    ADD CONSTRAINT `FK_CarPartListings_ToLogin` FOREIGN KEY (`LoginId`) 
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_CarPartListings_ToLogin` FOREIGN KEY (`ID`) 
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `CarPartListing`
-    ADD CONSTRAINT `FK_CarPartListings_ToRegion` FOREIGN KEY (`RegionId`)
-    REFERENCES `Region` (`RegionId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_CarPartListings_ToRegion` FOREIGN KEY (`ID`)
+    REFERENCES `Region` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `CarPartListing`
-    ADD CONSTRAINT `FK_CarPartListings_ToImage` FOREIGN KEY (`ImageId`)
-    REFERENCES `Image` (`ImageId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_CarPartListings_ToImage` FOREIGN KEY (`ID`)
+    REFERENCES `Image` (`ID`) ON DELETE CASCADE;
 
 -- Comment
 ALTER TABLE `Comment`
-    ADD CONSTRAINT `FK_Comments_ToCarPartListing` FOREIGN KEY (`PartListingId`)
-    REFERENCES `CarPartListing` (`CarPartListingId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Comments_ToCarPartListing` FOREIGN KEY (`ID`)
+    REFERENCES `CarPartListing` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `Comment`
-    ADD CONSTRAINT `FK_Comments_ToCarListing` FOREIGN KEY (`ListingId`)
-    REFERENCES `CarListing` (`CarListingId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Comments_ToCarListing` FOREIGN KEY (`ID`)
+    REFERENCES `CarListing` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `Comment`
-    ADD CONSTRAINT `FK_Comments_ToLogin` FOREIGN KEY (`LoginId`)
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Comments_ToLogin` FOREIGN KEY (`ID`)
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 -- Inbox
 ALTER TABLE `Inbox`
-    ADD CONSTRAINT `FK_Table_ReceiverLogin` FOREIGN KEY (`ReceiverLoginId`)
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Table_ReceiverLogin` FOREIGN KEY (`ID`)
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `Inbox`
-    ADD CONSTRAINT `FK_Table_SenderLogin` FOREIGN KEY (`SenderLoginId`)
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Table_SenderLogin` FOREIGN KEY (`ID`)
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 -- Profile
 ALTER TABLE `Profile`
-    ADD CONSTRAINT `FK_Profile_Login` FOREIGN KEY (`LoginId`)
-    REFERENCES `Login` (`LoginId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Profile_Login` FOREIGN KEY (`ID`)
+    REFERENCES `Login` (`ID`) ON DELETE CASCADE;
 
 -- Wishlist
 ALTER TABLE `Wishlist`
-    ADD CONSTRAINT `FK_Wishlist_ToCarPartListing` FOREIGN KEY (`PartListingId`)
-    REFERENCES `CarPartListing` (`CarPartListingId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Wishlist_ToCarPartListing` FOREIGN KEY (`ID`)
+    REFERENCES `CarPartListing` (`ID`) ON DELETE CASCADE;
 
 ALTER TABLE `Wishlist`
-    ADD CONSTRAINT `FK_Wishlist_ToCarListing` FOREIGN KEY (`ListingId`)
-    REFERENCES `CarListing` (`CarListingId`) ON DELETE CASCADE;
+    ADD CONSTRAINT `FK_Wishlist_ToCarListing` FOREIGN KEY (`ID`)
+    REFERENCES `CarListing` (`ID`) ON DELETE CASCADE;
